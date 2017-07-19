a2c6f6a2e8deba92b8a0f8b216fc23cf415e2cd2
ninad sangale <sangaleninad1891@gmail.com>
2017-07-18 20:47:52 -0400

    password hasher

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -40,4 +41,8 @@ class User extends Entity
     protected $_hidden = [
         'password'
     ];
+
+    protected function _setPassword($password){
+      return (new DefaultPasswordHasher)->hash($password);
+    }
 }
